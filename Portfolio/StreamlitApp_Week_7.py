ac0a56c07f36640f81d3330cc5b9e4a9a7730004
tjpreiss <t.j.preiss@tcu.edu>
2026-03-26 16:53:23 -0500

    Update StreamlitApp_Week_7.py

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -59,8 +59,8 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_pair.shap',
         "pipeline": 'finalized_pair_model.tar.gz',
-        "keys": ["LHX", "MPWR"],
-        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["LHX", "MPWR"]]
+        "keys": ["LHX", "INCY"],
+        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["LHX", "INCY"]]
 }
 
 def load_pipeline(_session, bucket, key):
