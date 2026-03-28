09bbf72bd3c0429c08f546cd06e05ed54d5b6425
tjpreiss <t.j.preiss@tcu.edu>
2026-03-28 18:01:01 -0500

    Change model input keys in MODEL_INFO
    Updated input keys in MODEL_INFO from ['LHX', 'INCY'] to ['LHX', 'ACN'] for model predictions.

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -60,7 +60,7 @@ MODEL_INFO = {
         "explainer": 'explainer_pair.shap',
         "pipeline": 'finalized_pair_model.tar.gz',
         "keys": ["LHX", "INCY"],
-        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["LHX", "INCY"]]
+        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["LHX", "ACN"]]
 }
 
 def load_pipeline(_session, bucket, key):
