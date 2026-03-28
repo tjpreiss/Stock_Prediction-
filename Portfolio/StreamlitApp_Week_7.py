fc1ffad079a3074efeed3d4444a2429f9db10d50
tjpreiss <t.j.preiss@tcu.edu>
2026-03-28 18:24:20 -0500

    Refactor imports and improve session management

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -59,7 +59,7 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_pair.shap',
         "pipeline": 'finalized_pair_model.tar.gz',
-        "keys": ["LHX", "INCY"],
+        "keys": ["LHX", "ACN"],
         "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["LHX", "ACN"]]
 }
 
